3a4d60cc0389b57c9b467b53b4f0fb2d6b2c9f0c
Dave McLain <dmclain@texastribune.org>
2012-03-27 13:56:30 -0500

    allow for passing render_video args manually

--- armstrong/core/arm_content/templatetags/content_helpers.py
+++ armstrong/core/arm_content/templatetags/content_helpers.py
@@ -1,3 +1,5 @@
+import urlparse
+
 from django import template
 from armstrong.core.arm_content.images.sorl import get_preset_thumbnail
 from armstrong.core.arm_content.images.presets import get_preset_args
@@ -11,5 +13,8 @@ def thumbnail(value, arg):
 
 @register.filter
 def render_video(value, arg):
-    args = get_preset_args(arg)
+    if arg.find('=') == -1:
+        args = get_preset_args(arg)
+    else:
+        args = urlparse.parse_qs(arg)
     return value.backend.embed(value, **args)
